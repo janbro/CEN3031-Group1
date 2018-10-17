7aa03ad28ad4f17d5d4758a72020a8583f85bfaf
Alejandro Munoz <amunoz797@gmail.com>
2018-10-17 11:25:40 -0400

    Updated ui and added backend service

---
+++ parking-picker-ui/src/app/services/backend.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class BackendService {
+
+  constructor() { }
+
+  public getAllGarages() {
+      return '';
+  }
+
+  public getGarages(decals) {
+      return decals;
+  }
+
+}
