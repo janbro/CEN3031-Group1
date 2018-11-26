61e2e308b20bbfb6fc73dd1051cdb87ba2c1d11f
Alejandro Munoz <amunoz797@gmail.com>
2018-11-26 16:26:59 -0500

    Added permissions endpoint

--- parking-picker-ui/src/app/services/backend.service.ts
+++ parking-picker-ui/src/app/services/backend.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 @Injectable()
 export class BackendService {
-mapboxConfigUrl = 'http://parking-picker.herokuapp.com/api/garages/mapbox';
+    mapboxConfigUrl = 'http://parking-picker.herokuapp.com/api/garages/mapbox';
+    permissionsConfigUrl = 'http://parking-picker.herokuapp.com/api/decals/';
+  
     constructor(private http: HttpClient) { }
 
     public getGaragesMapbox() {
@@ -11,6 +13,10 @@ mapboxConfigUrl = 'http://parking-picker.herokuapp.com/api/garages/mapbox';
     
     public getFilteredGaragesMapbox(permissions) {
         return this.http.post(this.mapboxConfigUrl, {decals:permissions});
+    }
+  
+    public getFilteredDecalPermissions(decals) {
+        return this.http.post(this.permissionsConfigUrl, {decals: decals});
     }
 
     public getAllGarages() {
