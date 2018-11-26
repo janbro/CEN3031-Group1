3b6331ff258bc5233b44e142ce3d553080f07676
Alejandro Munoz <amunoz797@gmail.com>
2018-11-26 16:34:39 -0500

    Changed decals to use permissions endpoint

--- parking-picker-ui/src/app/navigation/navigation.component.ts
+++ parking-picker-ui/src/app/navigation/navigation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl} from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { BackendService } from '../services/backend.service';
 
 @Component({
   selector: 'app-navigation',
@@ -11,11 +12,13 @@ export class NavigationComponent implements OnInit {
   permissions = [];
   decalList: Object[] = [{name:'Green',value:"green"}, {name:'Park & Ride', value:'parkNRide'},{name:'Red 1', value:'red1'}, {name:'Red 3', value:'red3'}, {name:'Brown', value: 'brown'}, {name:'Disabled', value:'handicap'}, {name:'Motorcycle/Scooter', value:'scooter'}];
   decalList1: Object[] = [{name:'Gold',value:'gold'},{name: 'Silver', value:'silver'},{name: 'Official Business', value:'business'}, {name:'Orange', value:'orange'}, {name:'Blue', value:'blue'}, {name:'Medical Resident', value:'medRes'}, {name:'Staff Commuter', value:"staffComm"},{name: 'Disabled', value:'handicap'}, {name:'Carpool', value:'carpool'},{name: 'Motorcycle/Scooter', value:'scooter'}];
-  constructor() { }
+  constructor(private backendService: BackendService) { }
 
   updateSettings()
   {
-    this.permissions = this.decals.value;
+    his.backendService.getFilteredDecalPermissions(this.decals.value).subscribe((permissions) => {
+      this.permissions = permissions;
+    });
   }
 
   ngOnInit() {
